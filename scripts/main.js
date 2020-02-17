0a94aacbcc8f9d11ceebcde70d5bfb48d626b550
Ben Miller <elementmiller@gmail.com>
2020-02-17 11:53:31 +0100

    1er
    Premier algo

---
+++ scripts/main.js
@@ -0,0 +1,19 @@
+var joursdelasemaine = String(prompt('choisissez un jour'));
+
+switch (joursdelasemaine) {
+	case 'Lundi':
+		alert('Lundi des patates');
+		break;
+
+	case 'Mardi':
+		alert('Mardi des patates');
+		break;
+
+	case 'Mercredi':
+		alert('Mercredi des patates aussi');
+		break;
+
+	default:
+		alert('boh surement des patates');
+		break;
+}
