178de7dd4a1bf7a1f881c38a152c4ac72d414f4b
Ben Miller <elementmiller@gmail.com>
2020-02-21 14:34:16 +0100

    Update main.js
    TP âge en premier
    TP prénom en second

--- scripts/main.js
+++ scripts/main.js
@@ -17,3 +17,37 @@ switch (Age) {
 		alert("Oh vous avez " +Age+ " ans" );
 		break;
 }
+
+
+
+var Prenom = String(prompt("Comment vous appellez-vous ?"));
+
+switch (Prenom) {
+	case "sarah":
+		alert("Sarah Connor ?");
+		break;
+	
+	case "Siri":
+		alert("Siri ? vraiment ? géralt vous cherche !");
+		break;
+
+	case "Scarlett":
+		alert("Scarlett Johansson ! Un autographe s'il vous plait !");
+		break;
+
+	case "Audrey":
+		alert("Bonjour Audrey, aujourd'hui nini a essayé de manger une saucisse de poulet, heureusement j'ai pu le stopper");
+		break;
+
+	case "Nico":
+		alert("Retourne bosser faignasse !");
+		break;
+
+	case "Peter":
+		alert("Bonjour Spider Man !");
+		break;
+
+	default:
+		alert("Oh! c'est un joli prénom");
+		break;
+}
